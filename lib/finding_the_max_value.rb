0a492e8f3dd1a61c0b695e4358097d7aeaeb1494
Daniel Irwin <dirwin123@gmail.com>
2021-01-08 22:17:52 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,9 +1,6 @@
 def find_max_value(array)
-  current_max = 0 
+  max = 0 
   array.length.times do |i|
-    if current_max < array[i] do
-      current_max = array[i]
-    end
-  end
-  current_max
+    if 
+  max
 end
